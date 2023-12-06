8da3fe72ee3020f48f746a5fbba651f2062e7768
Gregor Mustang <110684118+gregor-shkashekwa@users.noreply.github.com>
2023-12-07 04:16:38 +0700

    Add files via upload

---
+++ site_resume (2)/site_resume (2)/site_resume/script.js
@@ -0,0 +1,51 @@
+function clearForm() {
+    document.getElementById('name').value = '';
+    document.getElementById('phone').value = '';
+    document.getElementById('message').value = '';
+}
+  
+function sendMessage() {
+
+    var name = document.getElementById('name').value;
+    var phone = document.getElementById('phone').value;
+    var message = document.getElementById('message').value;
+
+    // if (phone.value.length > 11) {
+    //     phone.value = phone.value.slice(0,11); 
+    //     return phone.value;
+    // }
+
+    // if (message.value.length > 150) {
+    //     message.value = message.value.slice(0,150); 
+    //     return phone.value;
+    // }
+
+    // if (name.value.length > 50) {
+    //     name.value = name.value.slice(0,50); 
+    //     return name.value;
+    // }
+
+    if (name.trim() == '' || phone.trim() == '' || message.trim() == '') {
+        alert('Пожалуйста, заполните все поля!');
+    }
+    else{
+        var telegramToken = '6579830229:AAEDa4tMllPXWPCZOdpXNGgd_DNSnUEb-10';
+        var chatId = '-1002036213924';
+  
+        var url = `https://api.telegram.org/bot${telegramToken}/sendMessage`;
+        var data = `chat_id=${chatId}&text=${encodeURIComponent(`Name: ${name}\nPhone: ${phone}\nMessage: ${message}`)}`;
+  
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  
+        xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            clearForm();
+            alert('Сообщение отправлено успешно!');
+            }
+        };
+  
+    xhr.send(data);
+    }
+}
